00421cedbefa1798284e2a9f72238c2bfcf10fec
eunjinnnn <sally0680@naver.com>
2024-04-01 20:49:47 +0900

    floating choice, remove hardcoding

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,9 +3,10 @@
 # # Create your views here.
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
+
 
 def index(request):
     #가장 최근 질문 5개 보여줌
@@ -14,7 +15,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
